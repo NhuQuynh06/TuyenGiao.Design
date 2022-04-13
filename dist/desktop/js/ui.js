81c77f3f905d1a366cb530ccd8e99099f12fadd0
CPU60417\quynhttn3 <quynhtran0618@gmail.com>
2022-04-13 19:21:55 +0700

    'style'

--- dist/desktop/js/ui.js
+++ dist/desktop/js/ui.js
@@ -1,14 +1,35 @@
-// active nav-tab
-const $navTabLink = $('.nav-tab .nav-tab__link a');
-const $navTabContent = $('.nav-tab .nav-tab__content ');
+$(document).ready(function () {
+    // active nav-tab
+    const $navTabLink = $('.nav-tab .nav-tab__link a');
+    const $navTabContent = $('.nav-tab .nav-tab__content ');
 
-$(".nav-tab .nav-tab__link a").click(function (e) {
-    // INFO: prevent hash-jumping from anchor element
-    e.preventDefault();
-    const $this = $(e.target);
-    const chosenTab = $this.attr('data-link');
-    $navTabLink.removeClass('active');
-    $this.addClass('active');
-    $navTabContent.find('.tab-pane').removeClass('active')
-    $('.nav-tab .nav-tab__content .' + chosenTab).addClass('active');
+    $(".nav-tab .nav-tab__link a").click(function (e) {
+        // INFO: prevent hash-jumping from anchor element
+        e.preventDefault();
+        const $this = $(e.target);
+        const chosenTab = $this.attr('data-link');
+        $navTabLink.removeClass('active');
+        $this.addClass('active');
+        $navTabContent.find('.tab-pane').removeClass('active')
+        $('.nav-tab .nav-tab__content .' + chosenTab).addClass('active');
+    });
+
+    // slider-for, slider-nav
+
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: false,
+        centerMode: true,
+        focusOnSelect: true
+    });
+
 });
