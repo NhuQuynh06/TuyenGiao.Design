9954e9cad899ac333d6daca060844fb0b07c8bdf
CPU60417\quynhttn3 <quynhtran0618@gmail.com>
2022-05-16 18:11:42 +0700

    update style

--- dist/desktop/js/ui.js
+++ dist/desktop/js/ui.js
@@ -23,11 +23,28 @@ $(document).ready(function () {
         fade: true,
         asNavFor: '.slider-nav'
     });
+
     $('.slider-nav').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
         asNavFor: '.slider-for',
         dots: false,
+        centerMode: true,
+        focusOnSelect: true
+    });
+
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: true,
+        asNavFor: '.slider-nav',
+    });
+    $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: true,
         centerMode: true,
         focusOnSelect: true
     });
